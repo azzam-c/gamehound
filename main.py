a8c15582ef844056072222f885b9ac28ae5518ab
azzam <azzamchauhan6@gmail.com>
2026-06-25 18:31:25 -0400

    basic version complete, simply fetches data from steam API and displays.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import requests
+def run():
+    id=int(input('Enter steam app ID: '))
+    fetchReviews(id)
 def fetchReviews(id):
-    requests.get("https://store.steampowered.com/appreviews/{}?json=1".format(id))
+    url= f"https://store.steampowered.com/appreviews/{id}?json=1"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    scoredesc = data["query_summary"]["review_score_desc"]
+    print(f"General Feelings: {scoredesc}")
+    for review in data["reviews"]:
+        print("")
+        hours_at_post = review["author"]["playtime_at_review"]
+        print("---> " + review["author"]["personaname"] + ", with {} hours, says: ".format(hours_at_post))
+        print(review["review"])
+        print("--------------------------------------------------------------------------------------------------------------------------------------------------------------")
+fetchReviews(1778820)
+##  for dict in data:   ##tries to scan thru for each dictionary in data, but each data key is of a different type
+##      print(dict)
