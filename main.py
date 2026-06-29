e720defc823c0a0e2adae5bc072116e9322bdf8b
azzam <azzamchauhan6@gmail.com>
2026-06-29 17:08:58 -0400

    Fixed unwanted function nesting and created voteBalance function.

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import requests
 def run():
-    id=int(input('Enter steam app ID: '))
-    fetchReviews(id)
-def fetchReviews(id):
-    url= f"https://store.steampowered.com/appreviews/{id}?json=1"
+    app_id = int(input("Enter Steam app ID: "))
+    fetchReviews(app_id)
+def fetchReviews(app_id):
+    url = f"https://store.steampowered.com/appreviews/{app_id}?json=1"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
+    reviews = data["reviews"]
     scoredesc = data["query_summary"]["review_score_desc"]
     print(f"General Feelings: {scoredesc}")
-    for review in data["reviews"]:
-        print("")
-        hours_at_post = review["author"]["playtime_at_review"]
-        print("---> " + review["author"]["personaname"] + ", with {} hours, says: ".format(hours_at_post))
+    for review in reviews:
+        playtime_minutes = review["author"]["playtime_at_review"]
+        print()
+        print("---> " + review["author"]["personaname"] +
+              ", with {} minutes, says: ".format(playtime_minutes))
         print(review["review"])
-        print("--------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    def returnReviewText(id):
-        reviews = data["reviews"]
-        reviewTexts = []
-        for review in reviews:
-            reviewTexts.append(review["review"])
-        for text in reviewTexts:
-            print(text)
-fetchReviews(1778820)
-##  for dict in data:   ##tries to scan thru for each dictionary in data, but each data key is of a different type
-##      print(dict)
+        print("-" * 80)
+    returnReviewText(reviews)
+    voteBalance(reviews)
+def returnReviewText(reviews):
+    reviewTexts = []
+    for review in reviews:
+        reviewTexts.append(review["review"])
+    for text in reviewTexts:
+        print(text)
+
+def voteBalance(reviews):
+    positive_count = 0
+    negative_count = 0
+    for review in reviews:
+        if review["voted_up"]:
+            positive_count += 1
+        else:
+            negative_count += 1
+    print("Positive:", positive_count)
+    print("Negative:", negative_count)
+
+run()
